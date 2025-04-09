e4284b6c20be4b6b31bdff11b6351676094b9fa8
Dev Mukherjee <83952840+subho2002@users.noreply.github.com>
2025-04-09 13:50:53 +0530

    Add files via upload

---
+++ 16_Multiplication_Table.py
@@ -0,0 +1,13 @@
+# Solution-1
+num = int(input("Enter any number "))
+
+for i in range(1,11):
+    print(num,"X",i,"=",num*i)
+
+# Solution-2
+num = int(input("Enter any number "))
+i=1
+
+while i<=10:
+    print(num,"X",i,"=",num*i)
+    i+=1
