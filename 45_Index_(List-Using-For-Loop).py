bd01e74d813833417a2e4214fd9c6393e2387404
Dev Mukherjee <83952840+subho2002@users.noreply.github.com>
2025-04-18 12:12:21 +0530

    Add files via upload

---
+++ 45_Index_(List-Using-For-Loop).py
@@ -0,0 +1,12 @@
+# Solution-1
+
+l = [1,2,3,4,5]
+for index,value in enumerate(l):
+    print(index,"-",value)
+
+# Solution-2
+l = [6,7,8,9,10]
+
+for index in range(len(l)):
+    value = l[index]
+    print(index,"-",value)
