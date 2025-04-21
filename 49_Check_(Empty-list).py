a2d33a1fd43a961ed7a1b376b55ef30159945ce4
Dev Mukherjee <83952840+subho2002@users.noreply.github.com>
2025-04-21 14:54:51 +0530

    Add files via upload

---
+++ 49_Check_(Empty-list).py
@@ -0,0 +1,22 @@
+# Solution-1
+my_list = []
+if not my_list:
+    print("This list is empty")
+else:
+    print("This list is not empty")
+
+# Solution-2
+my_list_2 = []
+
+if  len(my_list_2) == 0:
+    print("This list is empty")
+else:
+    print("This list is not empty")
+
+# Solution-3
+my_list_3 = []
+
+if  my_list_3 == []:
+    print("This list is empty")
+else:
+    print("This list is not empty")
