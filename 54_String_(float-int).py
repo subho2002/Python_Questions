efdb2118d64f97d71586031a6e1d6ed2e02016db
Dev Mukherjee <83952840+subho2002@users.noreply.github.com>
2025-04-24 16:16:08 +0530

    Add files via upload

---
+++ 54_String_(float-int).py
@@ -0,0 +1,19 @@
+string = "123456789"
+
+print(string)
+print(type(string))
+
+int_str = int(string)
+
+print(int_str)
+print(type(int_str))
+
+float_str = float(string)
+
+print(float_str)
+print(type(float_str))
+
+int_float_str = int(float(string))
+
+print(int_float_str)
+print(type(int_float_str))
