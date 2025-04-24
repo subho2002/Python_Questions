2483b6d20c527812e9de12b24919604ed6cbf2ed
Dev Mukherjee <83952840+subho2002@users.noreply.github.com>
2025-04-24 16:15:54 +0530

    Add files via upload

---
+++ 53_Key_(Present-dictionaries).py
@@ -0,0 +1,8 @@
+num_bers = {1:2,3:4,5:6,7:8,9:10}
+
+num_user = int(input("Enter your number "))
+
+if num_user in num_bers:
+    print(num_user," is Present ")
+else:
+    print(num_user," is not present ")
