038eb9267f49c3684dc44c68ad70005f68398915
Dev Mukherjee <83952840+subho2002@users.noreply.github.com>
2025-05-12 23:54:51 +0530

    Add files via upload

---
+++ Day10_Python.py
@@ -0,0 +1,7 @@
+name = input("Enter any name you want ")
+print("name is ",name)
+
+numb1 = int(input("Enter any numb1 you want "))
+numb2 = int(input("Enter any numb2 you want "))
+
+print("Sum of num1 and num2 is ",numb1+numb2)
