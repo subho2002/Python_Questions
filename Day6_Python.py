7eca94eeb6a20cef9b7e1a63f44d395d4b42f661
Dev Mukherjee <83952840+subho2002@users.noreply.github.com>
2025-05-12 23:53:44 +0530

    Add files via upload

---
+++ Day6_Python.py
@@ -0,0 +1,16 @@
+a = complex(8,2)
+b = None
+c = False
+d = "String"
+e = 11
+f = 1.1
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
