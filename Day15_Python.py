593753fb3c6b3bd703ed3253f191ef1d5b430d11
Dev Mukherjee <83952840+subho2002@users.noreply.github.com>
2025-05-13 23:13:28 +0530

    Add files via upload

---
+++ Day15_Python.py
@@ -0,0 +1,10 @@
+import time 
+
+time_stamp = time.strftime('%H : %M : %S')
+print(time_stamp)
+time_stamp = time.strftime('%H')
+print(time_stamp)
+time_stamp = time.strftime('%M')
+print(time_stamp)
+time_stamp = time.strftime('%S')
+print(time_stamp)
