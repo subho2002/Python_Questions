c5ba9b0758682d9e1ab040331eb8e4a161951224
Dev Mukherjee <83952840+subho2002@users.noreply.github.com>
2025-05-13 23:12:55 +0530

    Add files via upload

---
+++ Day13_Python.py
@@ -0,0 +1,20 @@
+string = "!!!!! This is a string !!!!!"
+
+print(len(string))
+print(string)
+print(string.upper())
+print(string.lower())
+print(string.rstrip("!"))
+
+print(string.replace("is","was"))
+print(string.split("-"))
+print(string.capitalize())
+print(string.center(25))
+print(string.count("!"))
+
+print(string.endswith("s"))
+print(string.find("is"))
+print(string.index("This"))
+print(string.startswith("a"))
+
+print(string.swapcase())
