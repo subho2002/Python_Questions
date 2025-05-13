fb6e590422f3110ea5dc1ae7b04bf2ff780932ff
Dev Mukherjee <83952840+subho2002@users.noreply.github.com>
2025-05-13 23:12:18 +0530

    Add files via upload

---
+++ Day11_Python.py
@@ -0,0 +1,12 @@
+name = "Saatvik"
+ 
+fruit = "Mango"
+
+print(name," loves ",fruit)
+print(fruit[0])
+print(fruit[1])
+print(fruit[2])
+print(fruit[3])
+
+for char in fruit :
+    print("Characters : ",char)
