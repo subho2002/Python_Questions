9e42cba6c54285f289264474af0080b16da1c371
Dev Mukherjee <83952840+subho2002@users.noreply.github.com>
2025-05-15 13:03:10 +0530

    Add files via upload

---
+++ Day19_Python.py
@@ -0,0 +1,12 @@
+for i in range(1,15):
+    print("5 X ",i," = ",5*i)
+    if i == 10:
+        break
+
+
+for i in range(1,15):
+    if i == 10:
+        continue
+    print("5 X ",i," = ",5*i)
+
+    
