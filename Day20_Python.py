ba0aa3d99344fc149e930de77576305c94624f1c
Dev Mukherjee <83952840+subho2002@users.noreply.github.com>
2025-05-15 13:03:36 +0530

    Add files via upload

---
+++ Day20_Python.py
@@ -0,0 +1,13 @@
+def fun_1(a,b):
+    return (a+b)/2
+
+def fun_2(a,b):
+    return (a**b)
+
+a = int(input("Enter any anum "))
+b = int(input("Enter any bnum "))
+
+fun1 = fun_1(a,b)
+print(fun1)
+fun2 = fun_2(a,b)
+print(fun2)
