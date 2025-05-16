994d0c244744e6457ce03ac2fed82dcd1148694d
Dev Mukherjee <83952840+subho2002@users.noreply.github.com>
2025-05-16 13:46:39 +0530

    Add files via upload

---
+++ Day24_Python.py
@@ -0,0 +1,12 @@
+tuple = (1,2,3,1,4,1,5,1,6,1,7,1,8)
+
+print(type(tuple),tuple)
+print(len(tuple))
+print(tuple[0])
+print(tuple[6])
+print(tuple[-1])
+
+if 7 in tuple :
+    print(" present hai ")
+else:
+    print(" Absent ")
